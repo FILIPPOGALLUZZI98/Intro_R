8c1c5d3685612bc71e2c6548dc54ce4b6d330160
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-12 22:32:23 +0100

    Update 100.Librerie.R

--- 100.Librerie.R
+++ 100.Librerie.R
@@ -1,3 +1,20 @@
 # I pacchetti sono delle librerie di software
 # CRAN = Comprehensive R Archive Network, dove sono raccolti tutti i pacchetti R
+# Per installare ed usare i pacchetti dobbiamo importarli dall'esterno, quindi servono le virgolette
+install.packages("nomepacchetto")
+# Se un pacchetto è già installato allora dobbiamo richiamarlo ogni volta che vogliamo usarlo
+library(nomepacchetto)
+
+#######################################################################################################
+#######################################################################################################
+bei  ## 
+terra  ## Analisi Spaziale
+sdm  ## Species Distribution Model
+vegan  ## Vegetation Analysis
+spatstat  ## 
+devtools  ## 
+imageRy  ## Per installarlo: library(devtools); install_github("ducciorocchini/imageRy")
+viridis  ## 
+ggplot  ##
+ncdf4  ##
 
