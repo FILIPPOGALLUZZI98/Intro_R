0907cda2aeb22239750ae1c323078f994835ced5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-12 23:01:57 +0100

    Update 204.Data_Structures.R

--- 204.Data_Structures.R
+++ 204.Data_Structures.R
@@ -56,6 +56,11 @@ a%*%b	 ## Matrix multiplication
 
 #######################################################################################################
 #######################################################################################################
+# Per caricare dei dati da una cartella
+setwd("C:Percorso cartella")  ## 'setwd' --> set working directory
+# Poi, per passare dal nome del file ad un vettore
+a<-vect(nome_file)  ## La funzione è nel pacchetto 'terra'
+
 # Per ottenere informazioni su un dataset
 ?nome_dataset
 summary(A) # Informazioni sul dataset
@@ -74,23 +79,6 @@ a<-nome_dataset[[1]]  ## tra partentesi è indicata la posizone della variabile
 # Supponiamo che il dataset 'A' contenga le variabili 'a' e 'b', 
 # e che la varaiabile 'a' possa avere valori 0 e 1
 B<-A[A$a==1] # in questo modo assegno a 'B' solamente i valori 1 della variabile 'a' nel pacchetto 'A'
-
-#######################################################################################################
-#######################################################################################################
-# Per caricare dei dati da una cartella
-setwd("C:Percorso cartella")
-# 'setwd' --> set working directory
-# Oppure per cambiare la cartella basta andare 
-# su File/Change nella toolbar (in alto)
-
-#######################################################################################################
-#######################################################################################################
-# Se voglio trovare i nomi dei file all'interno di una cartella (o un pacchetto, per esempio 'sdm')
-library(sdm)
-nome_file<-system.file("external/nome.shp",package="dsm")
-# Poi, per passare dal nome del file ad un vettore
-a<-vect(nome_file) # la funzione è nel pacchetto 'terra'
-
 
 
 
