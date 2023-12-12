91477f6c58206e5f5a6dc217b4ed964435f539a5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-12 22:41:31 +0100

    Update 101.Intro.R

--- 101.Intro.R
+++ 101.Intro.R
@@ -1,26 +1,23 @@
 # R è case sensitive
-# A variable name must start with a letter and can be a combination of letters, digits, period(.) 
-# and underscore(_). If it starts with period(.), it cannot be followed by a digit. A variable name 
-# cannot start with a number or underscore (_). Variable names are case-sensitive. Reserved words 
-# cannot be used as variables (TRUE, FALSE, NULL, if...)
+# Il nome di una variabile deve iniziare con una lettera e può essere una combinazione di lettere, cifre, punto(.)
+# e carattere (_). Se inizia con punto(.), non può essere seguito da una cifra. Un nome di variabile
+# non può iniziare con un numero o (_). Parole riservate non possono essere utilizzati come variabili
 
 # To assign a value to a variable, use the '<-' sign
-a<-2
-# '<-' is preferred in most cases because the '=' operator can be forbidden in some context in R
+# R allows you to assign the same value to multiple variables in one line
+a,b,c<-2
+a<-b<-c<-2
 
 # You can also concatenate, or join, two or more elements, by using the 'paste()' function
 a<-"Mark"
 paste("My name is ",a)
-
-# R allows you to assign the same value to multiple variables in one line
-a,b,c<-2
-a<-b<-c<-2
 
 # ';' consente di fare due operazioni nella stessa riga
 a<-1; b<-1
 
 ###########################################################################################
 ###########################################################################################
+## DATA TYPES
 # We can use the 'class()' function to check the data type of a variable
 # Basic data types in R can be divided into the following types:
 # numeric
@@ -29,9 +26,7 @@ a<-1; b<-1
 # character (string)
 # logical (boolean) -> (TRUE or FALSE)
 # You can convert from one type of number to another with the following functions:
-as.numeric()
-as.integer()
-as.complex()
+as.numeric(); as.integer(); as.complex()
 
 # The 'min()' and 'max()' functions can be used to find the lowest or highest number in a set
 # The 'sqrt()' function returns the square root of a number
@@ -41,8 +36,8 @@ as.complex()
 
 ###########################################################################################
 ###########################################################################################
-# Strings are used for storing text
-# You can assign a multiline string to a variable
+## STRINGS
+# Strings are used for storing text. You can assign a multiline string to a variable
 a<-"My name
 is Mark"
 # R will add a '\n' at the end of each line break. This is called an escape character, and the n character
@@ -50,10 +45,9 @@ is Mark"
 # If you want the line breaks to be inserted at the same position as in the code, use the 'cat()' function
 a<-"My name is Mark"
 cat(a)
+
 # To find the number of characters in a string, use the 'nchar()' function
 # Use the 'grepl()' function to check if a character or a sequence of characters are present in a string
-a<-"My name is Mark"
-grepl(a)
 # To insert characters that are illegal in a string, you must use an escape character.
 # An escape character is a backslash \ followed by the character you want to insert.
 a <- "We are the so-called \"Vikings\", from the north."
@@ -62,6 +56,7 @@ a <- "We are the so-called \"Vikings\", from the north."
 
 ###########################################################################################
 ###########################################################################################
+## OPERATORS
 # R divides the operators in the following groups: Arithmetic operators, Assignment operators, 
 # Comparison operators, Logical operators, Miscellaneous operators.
 # Arithmetic operators are used with numeric values to perform common mathematical operations
