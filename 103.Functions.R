19715a65b87f7900ed4ce0d72a809ca7df2ef961
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-12 16:29:33 +0100

    Update 103.Functions.R

--- 103.Functions.R
+++ 103.Functions.R
@@ -9,3 +9,21 @@ f <- function(){
 # Information can be passed into functions as arguments.
 # Arguments are specified after the function name, inside the parentheses. You can add
 # as many arguments as you want, just separate them with a comma
+# Parameters or Arguments? The terms "parameter" and "argument" can be used for the same thing:
+# information that are passed into a function. From a function's perspective: A parameter is the 
+# variable listed inside the parentheses in the function definition. An argument is the value 
+# that is sent to the function when it is called.
+f<-function(x,y){
+  f(x,y,a)}
+# dove 'x', 'y' ed 'a' sono variabili/parametri e al posto di 'f(x,y,a)' va scritta la funzione che voglio
+f<-function(x,y){
+  x*y-3}
+f(2,3)
+# Ottengo come output 3
+
+
+
+
+
+
+
