292b8ea32599d0fee0cf312b61f9635208d25862
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-12 15:40:28 +0100

    Create 103.Functions.R

---
+++ 103.Functions.R
@@ -0,0 +1,10 @@
+# A function is a block of code which only runs when it is called. You can pass data, 
+# known as parameters, into a function. A function can return data as a result.
+
+# To create a function, use the 'function()' keyword
+f <- function(){
+  'argument'}
+
+# Information can be passed into functions as arguments.
+# Arguments are specified after the function name, inside the parentheses. You can add
+# as many arguments as you want, just separate them with a comma
