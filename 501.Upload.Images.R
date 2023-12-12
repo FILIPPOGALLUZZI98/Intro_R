2781c12044a94fa5e6090b244a19d74a93f00e04
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-12 23:05:41 +0100

    Update 501.Upload.Images.R

--- 501.Upload.Images.R
+++ 501.Upload.Images.R
@@ -3,4 +3,5 @@ setwd("D:Percorso_cartella")  ## 'setwd' --> set working directory
 # Se invece metto "D:\Percorso_cartella" non funziona, quindi fare attenzione a '/' dopo 'D:'
 
 img<-rast("NOME_FILE.ESTENSIONE")  ## rast() --> per importare l'immagine (raster data)
+# La funzione 'rast()' è nel pacchetto 'terra'
 # Su windows ricordare di aggiungere l'estensione alla fine perché la nasconde!!
