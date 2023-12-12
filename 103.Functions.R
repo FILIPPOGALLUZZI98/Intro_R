6120c76f51e5e8b3b42fdd3a31b1b4bd6f4ef86d
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-12 17:12:25 +0100

    Update 103.Functions.R

--- 103.Functions.R
+++ 103.Functions.R
@@ -1,5 +1,8 @@
-# A function is a block of code which only runs when it is called. You can pass data, 
-# known as parameters, into a function. A function can return data as a result.
+# Una fuunzione è un blocco di codice che esegue quando viene richiamato.
+# Posso usare dentro una funzione anche una variabile globale, cioè che è stata definita
+# fuori dalla funzione. If you create a variable with the same name inside a function, this
+# variable will be local, and can only be used inside the function. The global variable 
+# with the same name will remain as it was, global and with the original value.
 
 # To create a function, use the 'function()' keyword
 f <- function(){
@@ -7,14 +10,13 @@ f <- function(){
 # To call a function, use the function name followed by parenthesis
 f()
 
-# Information can be passed into functions as arguments.
-# Arguments are specified after the function name, inside the parentheses. You can add
-# as many arguments as you want, just separate them with a comma
-# Parameters or Arguments? The terms "parameter" and "argument" can be used for the same thing:
+# You can add as many arguments as you want, just separate them with a comma
+# The terms "parameter" and "argument" can be used for the same thing:
 # information that are passed into a function. From a function's perspective: A parameter is the 
 # variable listed inside the parentheses in the function definition. An argument is the value 
 # that is sent to the function when it is called.
 
+# Per più variabili
 f<-function(x,y){
   return(f(x,y,a))}
 # Dove al posto di f(x,y,a) scrivo la forma della funzione (con 'a' come parametro)
