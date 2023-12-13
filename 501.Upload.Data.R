e383aabb1c0da29c0b306e58e64e5fddfd853933
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-13 22:37:47 +0100

    Update and rename 501.Upload.Images.R to 501.Upload.Data.R

--- 501.Upload.Data.R
+++ 501.Upload.Data.R
@@ -1,17 +1,21 @@
-# Per caricare un'immagine da una cartella
+# Per caricare dei dati da una cartella
 setwd("D:Percorso_cartella")  ## 'setwd' --> set working directory
 # Se invece metto "D:\Percorso_cartella" non funziona, quindi fare attenzione a '/' dopo 'D:'
+# In R, the backslash is the “escape” symbol, which is followed by another symbol to indicate a special character
 
-img<-rast("NOME_FILE.ESTENSIONE")  ## rast() --> per importare l'immagine (raster data)
-# La funzione 'rast()' è nel pacchetto 'terra'
-# Su windows ricordare di aggiungere l'estensione alla fine perché la nasconde!!
+# Poi, per utilizzare i dati devo trasformarli in un tipo di dato che R può interpretare
+# SU WINDOWS RICORDARE DI AGGIUNGERE L'ESTENSIONE PERCHE LA NASCONDE!!
+a<-read.cvs(nome_file.ext)  ## Per leggere file excel
+a<-vect(nome_file.ext)  ## La funzione è nel pacchetto 'terra'
+img<-rast("nome_file.ext")  ## rast() è nel pacchetto 'terra'
 
 # Per plottare soltanto una parte dell'immagine
 # ext<-c(m_lon,M_lon,m_lat,M_lat)
 # crop(nome,ext)
 
-
 # imageRy
 im.list() # This function produces a character vector of the names of files stored in the 'imageRy' package directory.
 im.import() # This function imports a chosen image stored in the 'imageRy' package.
 # im.plotRGB -->This function plots a RGB image from a SpatRaster object
+
+
