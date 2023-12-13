762ee06928d3b3f03132c932175d86985894f9c4
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-13 22:52:47 +0100

    Update 502.Create_Data.R

--- 502.Create_Data.R
+++ 502.Create_Data.R
@@ -1,5 +1,5 @@
 # Supponiamo di creare un dataframe 'd'
 # Per fare un file excel
-write.csv(d, "test.csv", row.names=FALSE)
+write.csv(d, "nome.csv", row.names=FALSE)
 # Per fare un file di lettura
-write.table(d, "test.dat", row.names=FALSE)
+write.table(d, "nome.dat", row.names=FALSE)
