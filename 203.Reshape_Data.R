9f440cb16935b6fc0e8d30525416bce0d007264f
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-13 19:44:36 +0100

    Update 203.Reshape_Data.R

--- 203.Reshape_Data.R
+++ 203.Reshape_Data.R
@@ -3,8 +3,7 @@ reshape()
 # This function reshapes a data frame between ‘wide’ format (with repeated measurements in 
 # separate columns of the same row) and ‘long’ format (with the repeated measurements in separate rows)
 
-
-## Arguments
+##  ARGUMENTS
 # data--> data frame
 # varying--> names of sets of variables in the wide format that correspond to single variables in long format
 # (‘time-varying’). This is canonically a list of vectors of variable names, but it can optionally be a matrix
@@ -19,40 +18,43 @@ reshape()
 
 ###########################################################################################################
 ###########################################################################################################
-# Let's consider this example
+##  ESEMPIO 1 ---> Da "largo" a "lungo"
 dd <- read.csv('https://stats.idre.ucla.edu/stat/data/hsb2.csv')
 head(dd); dim(dd)
 # Abbiamo 11 colonne con i nomi(id, female, race, ses, ...) e 200 righe (valori)
 # Per semplicità scegliamo soltanto una parte del dataset
 d <- dd[1:3, c(1:2, 8:9)]  ## Prime tre righe e colonne 1,2,8,9
-
-# Supponiamo di voler incolonnare insieme due variabili 'math' e 'writing'
-# Definisco una nuova variabile che contiene 'write' e 'math' 
-wvars <- c("write", "math")
-x<-reshape(d, varying=wvars, v.names="score", direction = "long")
-# 'varying' è l'argomento che indica quali variabili voglio unire
-# 'v.names' è il nome che do a questa nuova variabile formata dall'unione
-# 'direction' è la direzione lungo cui voglio unire
-# 'x' contiene due nuove variabili: 'time' e 'score'
-# 'time' definisce a quale delle due corrisponde lo 'score' (1==write, 2==math)
-# Per cambiare nome a 'time'
-x <- reshape(d, varying=wvars, v.names="score", times=wvars, timevar = "subject", direction = "long")
-# Per eliminare i nomi delle righe a sx
+# Questo è un dataset 'largo'. Per passare ad un dataset 'lungo' allora vogliamo incolonnare due variabili
+# Scegliamo 'write' e 'math' e creiamo un vettore con queste due nuove variabili
+nvect<-c("write", "math")
+# In questo caso vogliamo che le variabili 'math' e 'write' siano incolonnate una sopra l'altra
+# Quindi su 'varying' mettiamo la nuova variabile e la chiamiamo 'score'. Dobbiamo anche specificare
+# qual'è la direzione che vogliamo usare, in questo caso 'long'
+x <- reshape(d, varying=nvect, v.names="score", direction = "long")
+# Otteniamo così una lista con, al posto delle variabili 'math' e 'write', due nuove variabili
+# 'time' e 'score'. Sotto a 'score' ci sono i valori delle due variabili; sotto 'time' ci sono 
+# i valori per identificare a quale variabile corrisponde (1=write, 2=math)
+# Per cambiare nome alla variabile 'time' e per avere, al posto di 1/2 i nomi delle variabili
+# dobbiamo mettere al posto di 'times' il vettore con le due variabili e al posto di 'timevar' il nome
+x <- reshape(d, varying=nvect, v.names="score", times=nvect, timevar = "subject", direction = "long")
+# Le righe hanno anche, a sx, il nome di identificazione, per toglierlo
 rownames(x)<-NULL
-x
 
-# To go from long to wide we need to use 'idvar' and 'timevar'. 'idvar' identifies the variables that
-# identify a single case (e.g. a single person, or other observational unit). In contrast, 'timevar' 
-# indicates the variable that has the identifiers that become variables in the wide format. The 
-# remaining variable should have the values that match these new variables.
+###########################################################################################################
+###########################################################################################################
+##  ESEMPIO 2 ---> Da "lungo" a "largo"
+# To go from long to wide we need to use 'idvar' and 'timevar'. 'idvar' identifies the variables that identify
+# a single case. In contrast, 'timevar' indicates the variable that has the identifiers that become variables in
+# the wide format. The remaining variable should have the values that match these new variables
 w <- reshape(x, idvar=c("id", "female"), timevar = "subject", direction = "wide")
-# Note that w is identical to d, except for the last two column names that now have “score.” prepended
-# to them. We can change that like this
+w
+# Note that w is identical to d, except for the last two column names that now have 'score.' prependend to them
+# To change it
 cn <- colnames(w)
 cn <- gsub("score.", "", cn)
 colnames(w) <- cn
 w
-
+# A questo punto w=d, siamo tornati al caso di partenza
 
 
 
