26183569d76efd84d4eb268957a0ed9c000c5b8c
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-13 11:09:59 +0100

    Update 501.Upload.Images.R

--- 501.Upload.Images.R
+++ 501.Upload.Images.R
@@ -9,3 +9,9 @@ img<-rast("NOME_FILE.ESTENSIONE")  ## rast() --> per importare l'immagine (raste
 # Per plottare soltanto una parte dell'immagine
 # ext<-c(m_lon,M_lon,m_lat,M_lat)
 # crop(nome,ext)
+
+
+# imageRy
+im.list() # This function produces a character vector of the names of files stored in the 'imageRy' package directory.
+im.import() # This function imports a chosen image stored in the 'imageRy' package.
+# im.plotRGB -->This function plots a RGB image from a SpatRaster object
