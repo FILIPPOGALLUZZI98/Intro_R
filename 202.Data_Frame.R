9abef1b4cba50174c01f3a781ea8ddd9ce81b716
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-13 22:35:06 +0100

    Update 202.Data_Frame.R

--- 202.Data_Frame.R
+++ 202.Data_Frame.R
@@ -3,14 +3,27 @@
 # Data Frames can have different types of data inside it. While the first column 
 # can be character, the second and third can be numeric or logical. However, each
 # column should have the same type of data.
+# Un dataset è una collezione di dati
 
 # Per creare una dataframe a partire da 3 vettori 'a', 'b', 'c'
 data<-data.frame(
 Variabile_1=a, Variabile_2=b, Variabile_3=c)
 # In questo modo ottengo tre colonne con in cima il nome di ogni variabile
-summary(data)  ## Per Riassumere i dati 
-dim(); ncol(); nrow()  ## Per conoscere quante righe/colonne
 
+# Per ottenere informazioni sul dataset 
+?XXX  ## Informazioni generali
+dim(XXX)  ## Dimensione del dataset
+ncol(XXX); nrow(XXX)  ## Per conoscere quante righe/colonne
+names(XXX)  ## Nomi delle variabili
+rownames(XXX)  ## Nomi di ogni riga 
+colnames(XXX)  ## Nomi di ogni colonna 
+summary(XXX)  ## Statistical summary dei dati
+data(XXX)  ## Ci fa vedere il contenuto del dataset "XXX"
+head(XXX)  ## Ci fa vedere la prima parte del dataset
+tail(XXX)  ## Ci fa vedere l'ultima parte del dataset
+
+#######################################################################################################
+#######################################################################################################
 # Per accedere alle colonne possiamo usare diversi modi
 data[1]  ## Usando la posizione della colonna
 ##??## data[["Variabile_1"]]  ## Usando il nome della colonna
@@ -29,29 +42,6 @@ data<-data[,-c(n)]
 data_3<-rbind(data_1,data_2)  ## Verticalmente
 data_3<-cbind(data_1,data_2)  ## Orizzontalmente
 
-#######################################################################################################
-#######################################################################################################
-# Per caricare dei dati da una cartella
-setwd("D:Percorso_cartella")  ## 'setwd' --> set working directory
-# Se invece metto "D:\Percorso_cartella" non funziona, quindi fare attenzione a '/' dopo 'D:'
-# In R, the backslash is the “escape” symbol, which is followed by another symbol to indicate a special character
-# Poi, per passare dal nome del file ad un vettore
-a<-vect(nome_file)  ## La funzione è nel pacchetto 'terra'
-a<-read.cvs(nome_file)  ## Per leggere file excel
-
-# Un dataset è una collezione di dati
-# Supponiamo di avere un dataset 'AAA'
-# Per ottenere informazioni sul dataset 
-?AAA  ## Informazioni generali
-dim(AAA)  ## Dimensione del dataset
-names(AAA)  ## Nomi delle variabili
-rownames(AAA)  ## Nomi di ogni riga della prima colonna
-colnames(AAA)  ## Nomi di ogni colonna della prima riga
-summary(AAA)  ## Statistical summary dei dati
-data(AAA)  ## Ci fa vedere il contenuto del dataset "A"
-head(AAA)  ## Ci fa vedere la prima parte del dataset
-tail(AAA)  ## Ci fa vedere l'ultima parte del dataset
-
 # To find the lowest or highest value in a set
 min(AAA); max(AAA)
 # To find the index position of the max and min value in the table
@@ -65,7 +55,15 @@ rownames(AAA)[which.min(AAA$nome_variabile)]
 # e che la varaiabile 'a' possa avere valori 0 e 1
 B<-A[A$a==1] # in questo modo assegno a 'B' solamente i valori 1 della variabile 'a' nel pacchetto 'A'
 
-
+#######################################################################################################
+#######################################################################################################
+# Per caricare dei dati da una cartella
+setwd("D:Percorso_cartella")  ## 'setwd' --> set working directory
+# Se invece metto "D:\Percorso_cartella" non funziona, quindi fare attenzione a '/' dopo 'D:'
+# In R, the backslash is the “escape” symbol, which is followed by another symbol to indicate a special character
+# Poi, per passare dal nome del file ad un vettore
+a<-vect(nome_file)  ## La funzione è nel pacchetto 'terra'
+a<-read.cvs(nome_file)  ## Per leggere file excel
 
 
 
