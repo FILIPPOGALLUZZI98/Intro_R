27816c8423844dba67751ed9d021c92813d6a76c
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-13 11:07:51 +0100

    Update 303.Colors.R

--- 303.Colors.R
+++ 303.Colors.R
@@ -1,1 +1,6 @@
-
+# Se vogliamo cambiare i colori della mappa di densità
+cl<-colorRampPalette(c(“black”,”red”,”orange”,”yellow”))(100) 
+# Devo usare le virgolette altrimenti i colori non vengono riconosciuti; 
+# ho bisogno di un array dove mettere quali colori voglio utilizzare; 
+# (100) serve per ottenere un gradiente tra un colore all’altro, se fosse (4) non avrei un gradiente,
+# ma semplicemente un salto netto tra un colore e l’altro;
