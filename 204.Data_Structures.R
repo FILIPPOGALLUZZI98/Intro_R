3d2115c8f2658610205a55223fffe4322b528645
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-13 12:07:20 +0100

    Update 204.Data_Structures.R

--- 204.Data_Structures.R
+++ 204.Data_Structures.R
@@ -26,6 +26,7 @@ A<-matrix(a, nrow=n, ncol=m)
 # Per conoscere la lunghezza usiamo 'lenght()'
 # Per ordinare in ordine alfabetico o numerico gli item usiamo 'sort()'
 # Per sapere se un certo elemento appartiene al datatype usiamo '%in%'
+
 # Per accedere agli elementi usiamo a[n] con n posizione
 # Nel caso di un array multidimensionale a[n,m,k,...]
 # Per accedere a più elementi
@@ -38,7 +39,6 @@ a[c(1,3:6)]  ## Tutti tranne il 2
 # or the elements of the data (perhaps a matrix) in one of the list elements. There's a 
 # difference betwwen double brackets: a[n] and a[[n]] are different!!
 
-
 # Per cambiare un elemento
 a[n]<-nuovo_elemento ## In posizione 'n'
 a[n,m,k,...]<-nuovo_elemento  ## Per array multidimensionali
@@ -47,6 +47,7 @@ append(a, valore)  ## Questo lo aggiunge in automatico alla fine
 append(a, valore, after=n)  ## Questo lo aggiunge dopo la posizione 'n'
 # Per rimuovere un elemento usiamo i numeri negativi
 a<-a[-n]
+
 
 
 # OPERAZIONI SU MATRICI
@@ -60,46 +61,6 @@ a[,c(1,2,...,m)]  ## Per selezionare da 1 a m colonn
 a<-a[-c(n),]
 a<-a[,-c(m)]
 a%*%b	 ## Matrix multiplication
-
-#######################################################################################################
-#######################################################################################################
-# Per caricare dei dati da una cartella
-setwd("D:Percorso_cartella")  ## 'setwd' --> set working directory
-# Se invece metto "D:\Percorso_cartella" non funziona, quindi fare attenzione a '/' dopo 'D:'
-# In R, the backslash is the “escape” symbol, which is followed by another symbol to indicate a special character
-# Poi, per passare dal nome del file ad un vettore
-a<-vect(nome_file)  ## La funzione è nel pacchetto 'terra'
-
-# Un dataset è una collezione di dati
-# Supponiamo di avere un dataset 'AAA'
-# Per ottenere informazioni sul dataset 
-?AAA  ## Informazioni generali
-dim(AAA)  ## Dimensione del dataset
-names(AAA)  ## Nomi delle variabili
-rownames(AAA)  ## Nomi di ogni riga della prima colonna
-colnames(AAA)  ## Nomi di ogni colonna della prima riga
-summary(AAA)  ## Statistical summary dei dati
-data(AAA)  ## Ci fa vedere il contenuto del dataset "A"
-head(AAA)  ## Ci fa vedere la prima parte del dataset
-tail(AAA)  ## Ci fa vedere l'ultima parte del dataset
-
-# Per accedere ad una variabile
-a<-AAA$nome_variabile
-a<-AAA[[n]]  ## tra partentesi è indicata la posizone della variabile 
-
-# To find the lowest or highest value in a set
-min(AAA); max(AAA)
-# To find the index position of the max and min value in the table
-which.max(AAA); which.min(AAA) 
-# Or even better, combine 'which.max()' and 'which.min()' with 'rownames()'
-rownames(AAA)[which.max(AAA$nome_variabile)]
-rownames(AAA)[which.min(AAA$nome_variabile)]
-
-
-# Se dentro il dataset la variabile può assumere valori diversi ed io voglio selezionarne soltanto uno
-# Supponiamo che il dataset 'A' contenga le variabili 'a' e 'b', 
-# e che la varaiabile 'a' possa avere valori 0 e 1
-B<-A[A$a==1] # in questo modo assegno a 'B' solamente i valori 1 della variabile 'a' nel pacchetto 'A'
 
 
 
