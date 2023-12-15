c783800f38b218ec0920131c597d006224d195cf
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-15 14:31:55 +0100

    Create 105.Functions.py

---
+++ Introduzione_Python/105.Functions.py
@@ -0,0 +1,23 @@
+# A function is a block of code which only runs when it is called. You can pass data,
+# known as parameters, into a function. A function can return data as a result
+# In Python a function is defined using the 'def' keyword
+def funct(x, y, ...):
+  argument
+
+# To let a function return a value, use the 'return' statement
+def funct(x,y,...):
+  return F(x,y,...)  ## Espressione matematica
+
+# To call a function, use the function name followed by parenthesis
+funct(a, b, ...)
+
+
+
+
+
+
+
+
+
+
+
