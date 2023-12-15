5f3edcaa7cabc9eefc79b40494c8aa08ab30b5f4
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-15 15:16:00 +0100

    Update 100.Intro.py

--- Introduzione_Python/100.Intro.py
+++ Introduzione_Python/100.Intro.py
@@ -60,7 +60,21 @@ find()  ## Searches the string for a specified value and returns the position of
 index()  ## Searches the string for a specified value and returns the position of where it was found
 title()	 ## Converts the first character of each word to upper case
 
+#################################################################################################################
+#################################################################################################################
+## FUNCTIONS
+# A function is a block of code which only runs when it is called. You can pass data,
+# known as parameters, into a function. A function can return data as a result
+# In Python a function is defined using the 'def' keyword
+def funct(x, y, ...):
+  argument
 
+# To let a function return a value, use the 'return' statement
+def funct(x,y,...):
+  return F(x,y,...)  ## Espressione matematica
+
+# To call a function, use the function name followed by parenthesis
+funct(a, b, ...)
 
 
 
