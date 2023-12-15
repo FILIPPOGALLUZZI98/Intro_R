677c30f858ed49061816f97876563c0c037e29d6
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-15 14:32:13 +0100

    Create 201.Numpy_Intro.py

---
+++ Introduzione_Python/201.Numpy_Intro.py
@@ -0,0 +1,31 @@
+# NumPy è una libreria Python usata per lavorare con gli array
+# The array object in NumPy is called ndarray
+# NumPy arrays are stored at one continuous place in memory unlike lists, 
+# so processes can access and manipulate them very efficiently
+import numpy as np
+
+# To create an array
+a = np.array([1, 2, 3, 4, 5])
+
+# Più dimensioni
+a = np.array([1,2,3,...], [1,2,3,...], [], ...)
+a.ndim  ## Restituisce la dimensione dell'array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
