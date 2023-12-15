73f671a3befff9d86b79e69a0de4c82b431cef20
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-15 10:23:03 +0100

    Update 301.Scatter_Plots.R

--- 301.Scatter_Plots.R
+++ 301.Scatter_Plots.R
@@ -1,21 +1,54 @@
+# zoom(x) and clicking on the map twice --> To indicate where to zoom to
+# sel(x) --> To save a spatial subset to a new object
+# click(x) --> To interactively query a SpatRaster by clicking once or several times 
+# on a map plot
+
 # Per fare uno scatterplot usiamo la funzione 'plot()'
 # Il primo parametro specifica la coordinata 'x', il secondo 'y'
 plot(1,2)
 # Per plottare più punti possiamo usare un vettore
 plot(c(1,2,3,4,5),c(4,8,12,45,35))
-
-# Nome Assi e Titolo
-plot(a, main="TITOLO", xlab="NOME", ylab="NOME")
-# Colore
-plot(a, col="red")
-# Grandezza
+# Grandezza dei punti
 plot(a, cex=2)
-# Forma
+# Forma dei punti
 plot(a, pch=25)
+# Colore dei punti
+plot(a, col="colore")
 # Posso anche usare la grandezza diversa dei punti, in base al loro valore
 grandezza<-c(1,2,3,4)  ## Oppure anche una funzione di un vettore, come "grand<-1+ precip/500"
 plot(a, cex=grandezza)  ## Imposto la grandezza variabile in base al valore del punto
 
+
+# Per customizzare il grafico
+plot(x,  main="TITOLO", xlab="NOME", ylab="NOME", col=cl)
+# Per fare la legendaa
+legend("topright, col='XXX')
+# Se nella legenda voglio mostrare i cerchi variabili 
+leg <- c(100, 250, 500, 1000)  ## Creo un vettore con le dimensioni standard
+legend.psize <- 1+leg/500
+legend("topright", legend=leg, pch=20, pt.cex=legend.psize, col='XXX', bg='XXX')
+
+# Per aggiungere testo (per esempio etichette per ogni punto)
+label=c("Elem_1","Elem_2","Elem_3")
+text(a, label, pos="n")
+
+# Altre customizzazioni
+plot(x, mar=XXX, plg=list(x="topright", title="XXX", bty = "o"))
+# mar --> margini???
+# plg --> 'plot legend' --> x= position; title; bty=???
+
+# Per aggiungere una scala diversa da quella di default
+plot(x, axes=FALSE)  ## To remove deafault axe
+axis(1, at=c(nn,nn)); axis(1)  ## 1 si riferisce all'asse 'x'
+axis(2, at=c(nn,nn)); axis(2)  ## axis(n) per mostrare l'asse sul grafico
+
+# If you want to color a map it is probably easiest to use an attribute
+plot(x, "Var", col=cl)
+# You can request maps for multiple variables
+plot(x, c("Var_1", "Var_2"), col=cl)
+
+########################################################################################################
+########################################################################################################
 # Per comparare due grafici a dispersione usiamo la funzione 'points()' sotto al primo grafico
 plot(a)
 points(b)
@@ -24,18 +57,6 @@ line1 <- c(1,2,3,4,5,10)
 line2 <- c(2,5,7,8,9,10)
 plot(line1, type = "l", col = "blue")
 lines(line2, type="l", col = "red")
-
-# Per aggiungere testo (per esempio etichette per ogni punto)
-label=c("Elem_1","Elem_2","Elem_3")
-text(a, label, pos="n")
-
-# Per fare la legendaa
-legend("topright, col='XXX')
-# Se nella legenda voglio mostrare i cerchi variabili 
-leg <- c(100, 250, 500, 1000)  ## Creo un vettore con le dimensioni standard
-legend.psize <- 1+leg/500
-legend("topright", legend=leg, pch=20, pt.cex=legend.psize, col='XXX', bg='XXX')
-
 
 # Per fare una linea
 plot(1:10, type="l")  ## In cui abbiamo usato la funzione per serie di numeri
