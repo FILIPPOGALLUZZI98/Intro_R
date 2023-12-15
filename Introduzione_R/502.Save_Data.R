e3454869fcce941cabd26a863dd9e6181e6ae8a0
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-15 14:35:48 +0100

    Update 502.Save_Data.R

--- Introduzione_R/502.Save_Data.R
+++ Introduzione_R/502.Save_Data.R
@@ -1,5 +1,1 @@
-# Supponiamo di creare un dataframe 'd'
-# Per fare un file excel
-write.csv(d, "nome.csv", row.names=FALSE)
-# Per fare un file di lettura
-write.table(d, "nome.dat", row.names=FALSE)
+
