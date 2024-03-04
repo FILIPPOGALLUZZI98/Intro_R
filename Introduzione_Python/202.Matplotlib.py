dc315d3be0e64bee59d5c886d330f7c1d7534d40
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-04 10:59:26 +0100

    Create 202.Matplotlib.py

---
+++ Introduzione_Python/202.Matplotlib.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Per plottare una linea tra i punti (X,Y)
+plt.plot(X,Y)
+plt.show()
+# Se voglio plottare solo i punti
+plt.plot(xpoints, ypoints, 'o')  ## In questo caso sono mostraati come pallini pieni
+plt.show()
+# Vari marker: o; *; .; ,; x; X; +; P; v; <; >;  
+# Formato: -; :; --; -.
+
+
+
+
