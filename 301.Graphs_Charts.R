4a59870047a32b432184354d5e6ab6b0364a6f3a
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-19 10:05:59 +0100

    Update 301.Graphs_Charts.R

--- 301.Graphs_Charts.R
+++ 301.Graphs_Charts.R
@@ -2,7 +2,9 @@
 # sel(x) --> To save a spatial subset to a new object
 # click(x) --> To interactively query a SpatRaster by clicking once or several times on a map plot
 
-# ScatterPlots
+
+#############################################################################################################################
+####  SCATTER PLOTS  ######################################################################################################
 plot(x, 
      pch = 10,
      cex = 0.5,
@@ -35,6 +37,7 @@ axis(1, at=c(nn,nn)); axis(1)  ## 1 si riferisce all'asse 'x'
 axis(2, at=c(nn,nn)); axis(2)  ## axis(n) per mostrare l'asse sul grafico
 
 
+#############################################################################################################################
 ####  LINES, POLYGONS  ######################################################################################################
 # Per fare una linea
 plot(1:10, type="l")  ## In cui abbiamo usato la funzione per serie di numeri
@@ -52,46 +55,6 @@ perim()  # To get the perimeter of the polygon
 # Per cercare il centro di un poligono A
 centr<-centroids(A)
 points(centr, cex=6, pch=20, col='white')
-
-
-####  OTHER MAIPULATIONS  ####################################################################################################
-# Mostrare due grafici nella stessa immagine accanto
-par(mfrow=c(1,2)) 
-# Dove il primo sta per le righe e il secondo per le colonne. 
-plot(grafico1) 
-plot(grafico2) 
-# Con questo ottengo due grafici uno accanto all’altro.
-# Se voglio chiudere i grafici
-dev.off()
-
-# Per comparare due grafici a dispersione usiamo la funzione 'points()' sotto al primo grafico
-plot(a)
-points(b)
-
-# Per mostrare più di una linea usiamo 'lines()' sotto al primo grafico
-line1 <- c(1,2,3,4,5,10)
-line2 <- c(2,5,7,8,9,10)
-plot(line1, type = "l", col = "blue")
-lines(line2, type="l", col = "red")
-
-# Per produrre una matrice di scatterplots
-pairs(a)
-# https://statisticsglobe.com/r-pairs-plot-example/
-
-
-####  DENSITY MAPS  ####################################################################################################
-# Se abbiamo un dataset con vari punti nello spazio e vogliamo passare ad una mappa di densità
-density_map<-density(dataset)
-# A questo punto non stiamo più lavorando con punti (o vettori) ma stiamo usando i pixel (raster)
-# Se vogliamo sovrapporre la mappa dei punti a quella di densità (o qualsiasi altro plot)
-points(dataset)
-
-# Se vogliamo cambiare i colori della mappa di densità
-cl<-colorRampPalette(c("black","red","orange","yellow"))(100) 
-# Devo usare le virgolette altrimenti i colori non vengono riconosciuti; 
-# ho bisogno di un array dove mettere quali colori voglio utilizzare; 
-# (100) serve per ottenere un gradiente tra un colore all’altro, se fosse (4) non avrei un gradiente,
-# ma semplicemente un salto netto tra un colore e l’altro;
 
 
 #### PIE CHART  ####################################################################################################
@@ -120,6 +83,7 @@ legend("bottomright", label_1, fill = cl)
 # bottomright, bottom, bottomleft, left, topleft, top, topright, right, center
 
 
+#############################################################################################################################
 #### BAR CHART  ####################################################################################################
 # Bar charts can be displayed horizontally or vertically
 # The height or length of the bars are proportional to the values they represent
@@ -137,9 +101,67 @@ barplot(y, names.arg=x, density=10)
 barplot(y, names.arg=x, width=c(1,2,3,4))
 
 
+#############################################################################################################################
 #### HISTOGRAMS  ####################################################################################################
-hist()
+hist(x,
+breaks = 10, 
+freq = FALSE,
+col = "red", 
+main = "Titolo",
+xlab = "Asse x",
+ylab = "Asse y")
 
+
+#############################################################################################################################
+####  OVERLYING AND COMPARING  ####################################################################################################
+# Mostrare due grafici nella stessa immagine accanto
+par(mfrow=c(1,2)) 
+# Dove il primo sta per le righe e il secondo per le colonne. 
+plot(grafico1) ; plot(grafico2) 
+# Con questo ottengo due grafici uno accanto all’altro
+# Se voglio chiudere i grafici
+dev.off()
+
+# Per comparare due grafici a dispersione usiamo la funzione 'points()' sotto al primo grafico
+plot(a)
+points(b)
+
+# Overlying charts (per esempio su un istogramma)
+curve(dnorm(x, mean(dataset), sd = sd(dataset)),
+col = "red",
+lwd = 2,
+add = TRUE)
+
+# Per mostrare più di una linea usiamo 'lines()' sotto al primo grafico
+line1 <- c(1,2,3,4,5,10)
+line2 <- c(2,5,7,8,9,10)
+plot(line1, type = "l", col = "blue")
+lines(line2, type="l", col = "red")
+
+# kernell density estimator
+lines(density(dataset), col ="red", lwd = 2, adjust = 3)  ## adjust ??
+
+# Rug
+rug(dataset, lwd = 2, col = "red")
+
+# Per produrre una matrice di scatterplots
+pairs(a)
+
+
+#############################################################################################################################
+####  DENSITY MAPS  ####################################################################################################
+# Se abbiamo un dataset con vari punti nello spazio e vogliamo passare ad una mappa di densità
+density_map<-density(dataset)
+# A questo punto non stiamo più lavorando con punti (o vettori) ma stiamo usando i pixel (raster)
+# Se vogliamo sovrapporre la mappa dei punti a quella di densità (o qualsiasi altro plot)
+points(dataset)
+
+# Se vogliamo cambiare i colori della mappa di densità
+cl<-colorRampPalette(c("black","red","orange","yellow"))(100) 
+# Devo usare le virgolette altrimenti i colori non vengono riconosciuti; 
+# ho bisogno di un array dove mettere quali colori voglio utilizzare; 
+# (100) serve per ottenere un gradiente tra un colore all’altro, se fosse (4) non avrei un gradiente,
+# ma semplicemente un salto netto tra un colore e l’altro;
 
 
 
