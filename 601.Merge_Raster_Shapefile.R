0078dce8d5421186b659a54262e21ff457b848a5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-03-19 16:02:30 +0100

    Update 601.Merge_Raster_Shapefile.R

--- 601.Merge_Raster_Shapefile.R
+++ 601.Merge_Raster_Shapefile.R
@@ -87,8 +87,43 @@ ggplot(shp, aes(fill=spei_2022_08)) +
 
 #################################################################################################
 ####  VISUALIZZARE SERIE TEMPORALI  #############################################################
+# Portiamo i dati in un formato che sia consono per plottare con ggplot2, portando i dati da un formato
+# largo ad uno lungo
+plot.df <- reshape2::melt(spei_data, id.vars="region")
+
+# Creiamo la variabile 'date', utile per plottare l'indice SPEI-12 nel tempo
+plot.df$date <- zoo::as.Date(zoo::as.yearmon(substr(as.character(plot.df$variable), 7, 13), "%Y.%m"))
+
+# Plottiamo il risultato
+ggplot(subset(plot.df, region %in% c("Pordenone", "Milano", "Palermo", "Bologna")), 
+       # date on x axis, spei (stored in variable "value") on y axis, color (of outlines) based on spei, filling based on spei
+       aes(date, value, fill=value, col=value)) +   
+  # add a horizontal line at zero
+  geom_hline(yintercept=0) +                  
+  # add a horizontal, dotted (lty=3) line at -1.5 to visualize the threshold for severe drought
+  geom_hline(yintercept=-1.5, lty=3) +     
+  # split the plot into separate plots for each region, organized in two columns
+  facet_wrap(region~., ncol=2) +        
+  # nicer theme than standard theme
+  theme_bw() +         
+  # white background for province names
+  theme(strip.background=element_rect(fill="white")) +          
+  # y axis title
+  ylab("SPEI-12") +                                                                
+  # x axis title
+  xlab("") +                                                                       
+  # columns for SPEI in each month
+  geom_col() +                                                                      
+  # set color palette (for the outlines of the columns)
+  scale_fill_viridis_c(option="inferno", end  = 0.8) +                              
+  # set fill palette (for filling the columns)
+  scale_color_viridis_c(option="inferno", end = 0.8) +                              
+  # set legend title
+  labs(fill="SPEI-12",col="SPEI-12")  
 
 
+#################################################################################################
+####  VISUALIZZARE SERIE TEMPORALI  #############################################################
 
 
 
